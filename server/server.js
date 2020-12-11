dbb7193a0f5c92e4aaf445c0b737637e657c6ba3
Nicholas Tolle <nbtolle@gmail.com>
2020-12-10 20:14:45 -0700

    Put in temp endpoints

--- server/server.js
+++ server/server.js
@@ -6,35 +6,36 @@ const port = 80;
 
 app.use(express.static('public'));
 
-app.use('/songdata/', createProxyMiddleware({
-  target: 'http://52.37.102.63:3005',
+app.use('/api/reviews/', createProxyMiddleware({
+  target: 'http://localhost:3001',
   changeOrigin: true
 }));
 
-app.use('/relatedTracks/', createProxyMiddleware({
-  target: 'http://3.15.220.99:3001',
+//copy and paste for other routes
+app.use('/api/seller/', createProxyMiddleware({
+  target: 'http://localhost:3002',
   changeOrigin: true
 }))
 
-app.use('/artistBio/', createProxyMiddleware({
-  target: 'http://34.220.154.45:2000',
+app.use('/api/images/', createProxyMiddleware({
+  target: 'http://localhost:3003',
   changeOrigin: true
 }))
 
-app.use('/comments/', createProxyMiddleware({
-  target: 'http://52.14.128.124:4000',
+app.use('/api/products/', createProxyMiddleware({
+  target: 'http://localhost:3004',
   changeOrigin: true
 }))
 
-app.use('/hashtags/', createProxyMiddleware({
-  target: 'http://18.189.26.97:4001',
-  changeOrigin: true
-}))
+// app.use('/hashtags/', createProxyMiddleware({
+//   target: 'http://18.189.26.97:4001',
+//   changeOrigin: true
+// }))
 
-app.use('/users/', createProxyMiddleware({
-  target: 'http://18.218.58.9:4002/',
-  changeOrigin: true
-}))
+// app.use('/users/', createProxyMiddleware({
+//   target: 'http://18.218.58.9:4002/',
+//   changeOrigin: true
+// }))
 
 app.use('/:current', (req, res) => {
   res.sendFile(path.join(__dirname,'/public/index.html'));
