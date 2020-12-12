5b02ab27a463089190c48d397b5525631c983da9
Nicholas Tolle <nbtolle@gmail.com>
2020-12-12 11:08:08 -0700

    Fix smart proxy

--- server/server.js
+++ server/server.js
@@ -6,56 +6,15 @@ const port = 80;
 
 app.use(express.static('public'));
 
-app.use('/api', createProxyMiddleware({
-  target: 'http://localhost',
+app.use('/reviews', createProxyMiddleware({
+  target: 'http://localhost:3001/bundle-reviews-service.js',
   changeOrigin: true,
-  router: {
-    '/reviews': 'http://localhost:3001',
-    '/products': 'http://localhost:3004'
+  pathRewrite: {
+    '^/reviews': ''
   }
-}));
+}))
 
-// app.get('/products', createProxyMiddleware({
-//   target: 'http://localhost',
-//   changeOrigin: true,
-//   router: {
-//     '/reviews': 'http://localhost:3001',
-//     '/products': 'http://localhost:3004'
-//   }
-// }));
-
-// app.use('api/reviews', createProxyMiddleware({
-//   target: 'http://localhost:3001',
-//   changeOrigin: true
-// }));
-
-//copy and paste for other routes
-// app.use('/api/seller/', createProxyMiddleware({
-//   target: 'http://localhost:3002',
-//   changeOrigin: true
-// }))
-
-// app.use('/api/images/', createProxyMiddleware({
-//   target: 'http://localhost:3003',
-//   changeOrigin: true
-// }))
-
-// app.use('/api/products/', createProxyMiddleware({
-//   target: 'http://localhost:3004',
-//   changeOrigin: true
-// }))
-
-// app.use('/hashtags/', createProxyMiddleware({
-//   target: 'http://18.189.26.97:4001',
-//   changeOrigin: true
-// }))
-
-// app.use('/users/', createProxyMiddleware({
-//   target: 'http://18.218.58.9:4002/',
-//   changeOrigin: true
-// }))
-
-app.use('/products/:product_id', (req, res) => {
+app.get('/:id', (req, res) => {
   res.sendFile(path.join(__dirname,'../public/index.html'));
 })
 
